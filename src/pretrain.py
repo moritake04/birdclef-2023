24e5e6c515a3470e5e2488b81c34271a31339a46
moritake04 <flameentei1@gmail.com>
2023-04-14 06:52:02 +0900

    pretrain

--- src/pretrain.py
+++ src/pretrain.py
@@ -9,8 +9,6 @@ import torch
 import wandb
 import yaml
 from pytorch_lightning import seed_everything
-from sklearn.model_selection import StratifiedKFold
-from tqdm import tqdm
 
 from classifier import bird2023classifier
 
@@ -64,33 +62,6 @@ def train_and_predict(cfg, train_X, train_y, valid_X=None, valid_y=None):
         return valid_preds
 
 
-def one_fold(skf, cfg, train_X, train_y, fold_n):
-    print(f"[fold_{fold_n}] start")
-    seed_everything(cfg["general"]["seed"], workers=True)
-    train_indices, valid_indices = list(skf.split(train_X, train_X["primary_label"]))[
-        fold_n
-    ]
-    train_X_cv, train_y_cv = (
-        train_X.iloc[train_indices].reset_index(drop=True),
-        train_y.iloc[train_indices].reset_index(drop=True),
-    )
-    valid_X_cv, valid_y_cv = (
-        train_X.iloc[valid_indices].reset_index(drop=True),
-        train_y.iloc[valid_indices].reset_index(drop=True),
-    )
-
-    valid_preds = train_and_predict(cfg, train_X_cv, train_y_cv, valid_X_cv, valid_y_cv)
-
-    print(f"[fold_{fold_n}]")
-    padded_cmap_score = padded_cmap(valid_y_cv.values, valid_preds)
-    wandb.log({"padded_cmap_score": padded_cmap_score})
-
-    torch.cuda.empty_cache()
-    wandb.finish()
-
-    return valid_preds, padded_cmap_score
-
-
 def all_train(cfg, train_X, train_y):
     print("[all_train] start")
     seed_everything(cfg["general"]["seed"], workers=True)
@@ -117,11 +88,55 @@ def main():
     seed_everything(cfg["general"]["seed"], workers=True)
 
     # Read csv
-    train = pd.read_csv(f"{cfg['general']['input_path']}/train_metadata.csv")
-    train["secondary_labels"] = train["secondary_labels"].apply(ast.literal_eval)
+    xeno_am = pd.read_csv(
+        f"{cfg['general']['input_path']}/xenocanto/train_extended.csv"
+    )[:14684]
+    xeno_nz = pd.read_csv(
+        f"{cfg['general']['input_path']}/xenocanto/train_extended.csv"
+    )[14685:]
+    bird2020 = pd.read_csv(f"{cfg['general']['input_path']}/bird2020/train.csv")
+    bird2021 = pd.read_csv(
+        f"{cfg['general']['input_path']}/bird2021/train_metadata.csv"
+    )
+    bird2022 = pd.read_csv(
+        f"{cfg['general']['input_path']}/bird2022/train_metadata.csv"
+    )
+    bird2023 = pd.read_csv(f"{cfg['general']['input_path']}/train_metadata.csv")
     sample_submission = pd.read_csv(
         f"{cfg['general']['input_path']}/sample_submission.csv"
     )
+    # Set common columns
+    xeno_am["birdclef"] = "xeno"
+    xeno_nz["birdclef"] = "xeno"
+    bird2020["birdclef"] = "2020"
+    bird2021["birdclef"] = "2021"
+    bird2022["birdclef"] = "2022"
+    xeno_am["primary_label"] = xeno_am["ebird_code"]
+    xeno_nz["primary_label"] = xeno_nz["ebird_code"]
+    bird2020["primary_label"] = bird2020["ebird_code"]
+    xeno_am["filepath"] = xeno_am["primary_label"] + "/" + xeno_am["filename"]
+    xeno_nz["filepath"] = xeno_nz["primary_label"] + "/" + xeno_nz["filename"]
+    bird2020["filepath"] = bird2020["primary_label"] + "/" + bird2020["filename"]
+    bird2021["filepath"] = bird2021["primary_label"] + "/" + bird2021["filename"]
+    bird2022["filepath"] = bird2022["filename"]
+    bird2023["filepath"] = bird2023["filename"]
+    # Set filiname and concat
+    bird2023["filename"] = bird2023["filepath"].map(
+        lambda x: x.split("/")[-1].split(".")[0]
+    )
+    train = pd.concat(
+        [xeno_am, xeno_nz, bird2020, bird2021, bird2022], axis=0
+    ).reset_index(drop=True)
+    train["filepath"] = train["filepath"].map(lambda x: x.split(".")[0])
+    train["filename"] = train["filepath"].map(lambda x: x.split("/")[-1])
+    # Drop duplicates and set columns
+    nodup_idx = train[["filename", "primary_label", "author"]].drop_duplicates().index
+    train = train.loc[nodup_idx].reset_index(drop=True)
+    train = train[~train["filename"].isin(bird2023["filename"])].reset_index(drop=True)
+    train = train[
+        ["filepath", "filename", "primary_label", "secondary_labels", "birdclef"]
+    ]
+    train["secondary_labels"] = train["secondary_labels"].apply(ast.literal_eval)
     # Retrieve target
     birds = sample_submission.columns[1:]
     # Label encoding
@@ -143,46 +158,20 @@ def main():
     train_y = train.iloc[:, -264:]
     cfg["model"]["num_classes"] = len(train_y.columns)
 
-    if cfg["general"]["cv"]:
-        skf = StratifiedKFold(
-            n_splits=cfg["general"]["n_splits"],
-            shuffle=True,
-            random_state=cfg["general"]["seed"],
-        )
-        valid_cmap_list = []
-        for fold_n in tqdm(cfg["general"]["fold"]):
-            cfg["fold_n"] = fold_n
+    # train all data
+    cfg["fold_n"] = "all"
 
-            if args.resume_train and os.path.isfile(
-                f"{cfg['general']['output_path']}/weights/{cfg['general']['save_name']}/last_epoch_fold{fold_n}.ckpt"
-            ):
-                print("resume train")
-                cfg[
-                    "ckpt_path"
-                ] = f"{cfg['general']['output_path']}/weights/{cfg['general']['save_name']}/last_epoch_fold{fold_n}.ckpt"
-            else:
-                cfg["ckpt_path"] = None
-
-            _, valid_cmap = one_fold(skf, cfg, train_X, train_y, fold_n)
-            valid_cmap_list.append(valid_cmap)
-
-        valid_cmap_mean = np.mean(valid_cmap_list, axis=0)
-        print(f"cv mean pfbeta:{valid_cmap_mean}")
+    if args.resume_train and os.path.isfile(
+        f"{cfg['general']['output_path']}/pretrain/weights/cfg['general']['save_name']}/last_epoch.ckpt"
+    ):
+        print("resume train")
+        cfg[
+            "ckpt_path"
+        ] = f"{cfg['general']['output_path']}/pretrain/weights/{cfg['general']['save_name']}/last_epoch.ckpt"
     else:
-        # train all data
-        cfg["fold_n"] = "all"
+        cfg["ckpt_path"] = None
 
-        if args.resume_train and os.path.isfile(
-            f"{cfg['general']['output_path']}/weights/{cfg['general']['save_name']}/last_epoch.ckpt"
-        ):
-            print("resume train")
-            cfg[
-                "ckpt_path"
-            ] = f"{cfg['general']['output_path']}/weights/{cfg['general']['save_name']}/last_epoch.ckpt"
-        else:
-            cfg["ckpt_path"] = None
-
-        all_train(cfg, train_X, train_y)
+    all_train(cfg, train_X, train_y)
 
 
 if __name__ == "__main__":
